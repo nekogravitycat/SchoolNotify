a3fa56484e1a30a0ceb5d81010f0c76af414e5af
Gravity cat <29692485+nekogravitycat@users.noreply.github.com>
2021-09-12 14:43:29 +0000

    Updated the website

--- website.py
+++ website.py
@@ -1,11 +1,11 @@
-from flask import Flask
+from flask import Flask, render_template
 from threading import Thread
 
 app = Flask('')
 
 @app.route('/')
 def home():
-  return "hello world"
+  return render_template("index.html")
 
 def run():
   app.run(host = '0.0.0.0', port = 8080)
