55b994154239ae28ccb733332c4491d63a27da76
Gravity cat <29692485+nekogravitycat@users.noreply.github.com>
2022-05-02 10:49:51 +0800

    added incoming cmd monitor

--- mycmd.py
+++ mycmd.py
@@ -1,4 +1,3 @@
 def run(cmd: str):
-	print(f"received cmd: {cmd}")
 	print("cmd function is still under construction, nothing has runned")
 	return "ok, cmd function is still under construction, nothing has runned"
