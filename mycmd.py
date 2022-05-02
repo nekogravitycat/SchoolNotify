d49124d3a644efccfb8f540f2fd6916557a65205
Gravity cat <29692485+nekogravitycat@users.noreply.github.com>
2022-05-02 23:12:06 +0800

    added debug function in mycmd

--- mycmd.py
+++ mycmd.py
@@ -1,8 +1,12 @@
 from unilog import log
 import basic
+import schools
 
-def show_sch(school: str):
-	return
+def show_sch(id: str):
+	if(not schools.is_valid(id)):
+		return f"error: invaild school id '{id}'"
+	basic.debug([id])
+	return "done!"
 
 def run(line: str):
 	split = line.split()
@@ -14,7 +18,7 @@ def run(line: str):
 	args: list = split
 	
 	if(cmd == "show-sch"):
-		show_sch(args[0])
+		return show_sch(args[0])
 	else:
 		return f'error: unknown command {cmd}'
 	
