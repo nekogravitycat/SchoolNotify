feeaa641775784a8a2416a68d342a2e6cd788519
Gravity cat <29692485+nekogravitycat@users.noreply.github.com>
2022-05-02 23:34:17 +0800

    renamed debugging cmd

--- mycmd.py
+++ mycmd.py
@@ -2,11 +2,13 @@ from unilog import log
 import basic
 import schools
 
-def show_sch(id: str):
+
+def debug_sch(id: str):
 	if(not schools.is_valid(id)):
 		return f"error: invaild school id '{id}'"
 	basic.debug([id])
 	return "done!"
+
 
 def run(line: str):
 	split = line.split()
@@ -14,12 +16,10 @@ def run(line: str):
 	if(len(split) == 0):
 		return "error: empty input"
 		
-	cmd  = split.pop(0)
+	cmd = split.pop(0)
 	args: list = split
 	
-	if(cmd == "show-sch"):
+	if(cmd == "debug-sch"):
 		return show_sch(args[0])
 	else:
 		return f'error: unknown command {cmd}'
-	
-	return "ok, cmd function is still under construction, nothing has runned"
