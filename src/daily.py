a8286a505bab3866a85867bbc48c03b523c97f8f
Gravity cat <29692485+nekogravitycat@users.noreply.github.com>
2022-08-07 22:23:37 +0800

    Update daily.py

--- src/daily.py
+++ src/daily.py
@@ -1,10 +1,12 @@
 from src import basic
+from src.unilog import log
 
 
 def schedule_run() -> None:
 	""" Run news-gathering if it's Monday or Thursday """
 
 	weekday: int = basic.today().weekday()
+	log(f"schedule_run: weekday={weekday}")
 	# monday = 0, thursday = 3
 	if weekday == 0 or weekday == 3:
 		basic.run()
